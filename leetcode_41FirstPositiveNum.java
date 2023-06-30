49f3f9875b419683a7a0042a4e2892f6eef22b65
parkhi-goel <106884243+parkhi-goel@users.noreply.github.com>
2023-06-30 22:38:19 +0530

    Create leetcode_41FirstPositiveNum.java

---
+++ leetcode_41FirstPositiveNum.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class LeetCodeQues41_FindFirstPositiveNumber {
+public int firstMissingPositive(int[] nums) {
+ Arrays.sort(nums);
+ int b = 1;
+ for(int i = 0; i < nums.length; i++)
+ {
+ if(nums[i]==b)
+ b++;
+ }
+ return b;
+ }
+}
