e650d80bd498cbb967b0da2d1a4ecd73069b9179
parkhi-goel <106884243+parkhi-goel@users.noreply.github.com>
2023-07-04 23:12:08 +0530

    gas_station.java
     LEETCODE PROBLEM NUMBER:134

---
+++ gas_station.java
@@ -0,0 +1,29 @@
+class Solution {
+public static int canCompleteCircuit(int[] gas, int[] cost) {
+int i;
+int totalGas=0;
+int totalCost=0;
+for(i=0;i<gas.length;i++){
+totalGas +=gas[i];
+totalCost +=cost[i];
+}
+if(totalCost>totalGas)
+return -1;
+int currentGas=0;
+int start=0;
+for(i=0;i<gas.length;i++){
+currentGas +=gas[i] - cost[i];
+if(currentGas<0){
+currentGas=0;
+start=i+1;
+}
+}
+return start;
+}
+public static void main(String[] agrs){
+int[]gas={1,2,3,4,5};
+int[]cost={3,4,5,1,2};
+int ans=canCompleteCircuit(gas,cost);
+System.out.println(ans);
+}
+}
