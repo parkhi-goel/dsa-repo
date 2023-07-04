3a0bf0b8efbe09414a71a224b66f71c460740a4f
parkhi-goel <106884243+parkhi-goel@users.noreply.github.com>
2023-07-04 23:14:16 +0530

    MAJORITYELEMENT.java
    LEETCODE PROBLEM NUMBER:169

---
+++ MAJORITY ELEMENT LEETCODE PROBLEM NUMBER:169.java
@@ -0,0 +1,24 @@
+class Solution {
+public static int majorityElement(int[] nums) {
+int majority=nums[0];
+int count=1;
+for(int i=0;i<nums.length;i++){
+if(nums[i]==majority){
+count++;
+}
+else{
+count--;
+}
+if(count==0){
+majority=nums[i];
+count=1;
+}
+}
+return majority;
+}
+public static void main(String[] args){
+int arr[]={2,2,1,1,1,2,2};
+int ans = majorityElement(arr);
+System.out.println("majority Occuring element is"+ans);
+}
+}
